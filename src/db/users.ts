17bbca37d4f18d48ca15153eced689b5d9454078
Dan Stronge <dnlstronge@gmail.com>
2023-06-23 11:50:59 +0100

    [chore] - add actions to schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -4,6 +4,15 @@ const userSchema = new mongoose.Schema({
     username : {type: String, required: true},
     email: {type: String, required: true},
     authentication: {
-        password: {type: String, required: true}
+        password: {type: String, required: true, select: false},
+        salt: {type: String, select: false },
+        sessionToken: {type: String, select: false}
     }
 })
+
+export const userModel = mongoose.model("User", userSchema)
+
+/* actions */
+export const getUsers = () => userModel.find()
+export const getUserByEmail = (email: string) => userModel.findOne({email})
+export const getUserBySessionToken = (sessionToken: string) => userModel.findOne({sessionToken})
