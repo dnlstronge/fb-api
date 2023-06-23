73e85f7dafea4cb9b9d586bbc89192ffc4a3450e
Dan Stronge <dnlstronge@gmail.com>
2023-06-23 12:02:03 +0100

    [task]  complete user model & schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -24,4 +24,4 @@ export const getUserById = (id: string) => {
 export const createUser = (values: Record<string, any>) => new userModel(values).save()
 .then((user) => user.toObject())
 export const deleteUserById = (id: string) => userModel.findOneAndDelete({ _id: id })
-export const updateUserById = (id: string) => userModel.findByIdAndUpdate({ _id: id })
+export const updateUserById = (id: string, values: Record<string, any>) => userModel.findByIdAndUpdate(id, values)
